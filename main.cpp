5eb7ed20173bd9a6bdf9d13899a34a8632409ad6
Honoredcodes <honoredcodes@gmail.com>
2025-05-13 19:32:21 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -3,18 +3,25 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
+#include <filesystem>
+#include <fstream>
 
 // class DataStructures {Temp};
-
+namespace fs = std::filesystem;
 class schedular {
 protected:
     struct Process {
         int process_id, arrival_time, burst_time, priority;
         Process* next = nullptr;
+        Process* prev = nullptr;
     };
 
     struct Results {
         int waiting, turnaround, completion;
+        int totalturnaround, totalwaiting, totalcompletion;
+        long averageturnaround, averagewaiting, averagecompletion;
+        Results* next = nullptr;
+        Results* prev = nullptr;
     };
 
 public:
@@ -23,7 +30,9 @@ public:
         void enqueue() {}
         void initQueue() {}
         bool queueIsEmpty() {}
-        bool queueIsFull() {}
+        bool queueIsFull() {
+
+        }
     };
 
     struct Stack {
@@ -34,7 +43,44 @@ public:
         bool stackIsFull() {}
     };
 
-    struct LinkedList {};
+    struct filereaders {
+        bool read(const std::string filename, Process*& processObj) {
+            const std::string filepath = fs::path("logs") / filename;
+            std::ifstream file(filepath);
+            if (!file) return false;
+            Process* head = nullptr;
+            Process* tail = nullptr;
+            std::string line;
+            while (std::getline(file, line)) {
+                std::string delimiter = ":";
+                size_t pos = 0;
+                int i = 0;
+                Process* newProcess = new Process();
+                newProcess->next = nullptr;
+                while ((pos = line.find(delimiter)) != std::string::npos) {
+                    std::string token = line.substr(0, pos);
+                    if (i == 0) newProcess->process_id = std::stoi(token);
+                    else if (i == 1) newProcess->arrival_time = std::stoi(token);
+                    else if (i == 2) newProcess->burst_time = std::stoi(token);
+                    else if (i == 3) newProcess->priority = std::stoi(token);
+                    line.erase(0, pos + delimiter.length());
+                    i++;
+                }
+                newProcess->priority = std::stoi(line);
+                if (!head) {
+                    head = newProcess;
+                    tail = newProcess;
+                } else {
+                    tail->next = newProcess;
+                    tail = newProcess;
+                }
+            }
+            file.close();
+            processObj = head;
+            return true;
+        }
+        void write() {}
+    };
 };
 
 
